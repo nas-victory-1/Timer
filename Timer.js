5d460cd1ec3e92d5aa998dc79ba8e81f157b294e
Nasser Samiru <nasserjadagwa@gmail.com>
2024-09-26 12:49:07 +0000

    Finished with the interface, barely started on the js

---
+++ Timer.js
@@ -0,0 +1,9 @@
+
+// const startButton = document.querySelector('.start-button');
+const stopButton = document.querySelector('.stop-button');
+const resetButton = document.querySelector('.reset-button');
+
+
+function timerStart(){
+  document.querySelector('.time-paragraph').innerHTML = '01';
+}
