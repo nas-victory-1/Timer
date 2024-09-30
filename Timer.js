0cec9d4c7ece62bb72c71ad83a5e4f0cb92aa1cc
Nasser Samiru <nasserjadagwa@gmail.com>
2024-09-30 10:18:13 +0000

    Facilitated the change of the appearance of a button when clicked

--- Timer.js
+++ Timer.js
@@ -1,9 +1,9 @@
-// const startButton = document.querySelector('.start-button');
+const startButton = document.querySelector('.start-button');
 const stopButton = document.querySelector(".stop-button");
 const resetButton = document.querySelector(".reset-button");
 
 
-let secondSecondCounter = 1;
+let secondSecondCounter = 0;
   let firstSecondCounter = 0;
   let secondMinuteCounter = 0;
   let firstMinuteCounter = 0;
@@ -19,46 +19,46 @@ let secondSecondCounter = 1;
   
   
 function timerStart() {
-  setInterval(incrementer, 10);
+  let toggled = startButton.classList.contains('is-toggled');
+  if(toggled){
+    startButton.innerHTML = 'Start';
+    startButton.classList.remove('stop-button');
+    startButton.classList.remove('is-toggled');
+  }else{
+    startButton.innerHTML = 'Stop';
+    startButton.classList.add('stop-button');
+    startButton.classList.add('is-toggled');
+  }
+
+  setInterval(incrementer, 1000);
 
   function incrementer() {
 
-    timeIncrementer(secondSecondCounter, 9, firstSecondCounter);
+    /*timeIncrementer(secondSecondCounter, 9, firstSecondCounter);
     timeIncrementer(firstSecondCounter,6,secondMinuteCounter);
-    timeIncrementer(secondMinuteCounter, 10, firstMinuteCounter);
+    timeIncrementer(secondMinuteCounter, 10, firstMinuteCounter);*/
     
 
-    // if (secondSecondCounter >= 9) {
-    //   secondSecondCounter = -1;
-    //   firstSecondCounter++;
+    if (secondSecondCounter >= 9) {
+      secondSecondCounter = -1;
+      firstSecondCounter++;
 
-    //   if (firstSecondCounter >= 6) {
-    //     firstSecondCounter = 0;
-    //     secondMinuteCounter++;
+      if (firstSecondCounter >= 6) {
+        firstSecondCounter = 0;
+        secondMinuteCounter++;
 
-    //     if (secondMinuteCounter >= 10) {
-    //       secondMinuteCounter = 0;
-    //       firstMinuteCounter++;
-    //       firstMinute.innerHTML = firstMinuteCounter;
-    //     }
-    //     secondMinute.innerHTML = secondMinuteCounter;
-    //   }
-    //   firstSecond.innerHTML = firstSecondCounter;
-    // }
+        if (secondMinuteCounter >= 10) {
+          secondMinuteCounter = 0;
+          firstMinuteCounter++;
+          firstMinute.innerHTML = firstMinuteCounter;
+        }
+        secondMinute.innerHTML = secondMinuteCounter;
+      }
+      firstSecond.innerHTML = firstSecondCounter;
+    }
 
-    // secondSecondCounter++;
-    // secondSecond.innerHTML = secondSecondCounter;
+    secondSecondCounter++;
+    secondSecond.innerHTML = secondSecondCounter;
   }
 }
 
-function timeIncrementer(firstCounter, highestNumber, secondCounter){
-  if(firstCounter >= highestNumber){
-    firstCounter = 0;
-    secondCounter++;
-  }
-  firstMinute.innerHTML = firstMinuteCounter;
-  secondMinute.innerHTML = secondMinuteCounter;
-  firstSecond.innerHTML = firstSecondCounter;
-  secondSecond.innerHTML = secondSecondCounter;
- // return secondCounter; 
-}
