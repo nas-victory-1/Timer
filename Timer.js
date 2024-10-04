95cfc1f1d9d276de73f699263543cef0bc4a3f59
Nasser Samiru <nasserjadagwa@gmail.com>
2024-10-04 16:34:58 +0000

    Looks like I'm done. Optimization remaining

--- Timer.js
+++ Timer.js
@@ -4,21 +4,21 @@ const resetButton = document.querySelector(".reset-button");
 
 
 let secondSecondCounter = 0;
-  let firstSecondCounter = 0;
-  let secondMinuteCounter = 0;
-  let firstMinuteCounter = 0;
+let firstSecondCounter = 0;
+let secondMinuteCounter = 0;
+let firstMinuteCounter = 0;
 
-  const secondSecond = document.querySelector(".second-second");
+const secondSecond = document.querySelector(".second-second");
 
-  const firstSecond = document.querySelector(".first-second");
+const firstSecond = document.querySelector(".first-second");
 
-  const secondMinute = document.querySelector(".second-minute");
+const secondMinute = document.querySelector(".second-minute");
 
-  const firstMinute = document.querySelector(".first-minute");
+const firstMinute = document.querySelector(".first-minute");
 
-  let intervalID;
-  
+let intervalID;
 
+const pulsing = document.querySelector('.pulsing');
   
   
 function timerStart() {
@@ -29,15 +29,16 @@ function timerStart() {
     startButton.classList.add('is-toggled');
     document.querySelector('.timer-container').classList.add('started-timer');
     document.querySelector('.pulsing').classList.add('pulsing-active');
+    playAnimation();
     intervalID = setInterval(incrementer, 100);
     
   }else{
     startButton.innerHTML = 'Start';
     startButton.classList.remove('stop-button');
     startButton.classList.remove('is-toggled');
-    document.querySelector('.timer-container').classList.remove('started-timer');
-    document.querySelector('.pulsing').classList.remove('pulsing-active');
-
+    // document.querySelector('.timer-container').classList.remove('started-timer');
+    //document.querySelector('.pulsing').classList.remove('pulsing-active');
+    pauseAnimation();
     clearInterval(intervalID);
     
   }
@@ -89,4 +90,11 @@ function resetTimer(){
   secondSecondCounter = 0;
   firstMinuteCounter = 0;
   firstSecondCounter = 0;
+}
+
+function pauseAnimation(){
+  pulsing.style.animationPlayState = 'paused';
+}
+function playAnimation(){
+  pulsing.style.animationPlayState = 'running';
 }
